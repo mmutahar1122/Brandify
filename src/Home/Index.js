809f64319ede4b32b6a924ebffbbf6b0a114639f
M. Mutahar <mmutahar38@gmail.com>
2023-09-16 20:00:55 -0600

    brandify name correct

--- src/Home/Index.js
+++ src/Home/Index.js
@@ -6,7 +6,7 @@ const Home = () => {
     
     <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
     <div className="hero-container" data-aos="fade-in">
-      <h1>Welcome To Bradify</h1>
+      <h1>Welcome To Brandify</h1>
       {/* <h2>I'm Designer</h2> */}
       <p><span className="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
     </div>
